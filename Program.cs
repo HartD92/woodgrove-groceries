3b1e2703507d874e27af21eff398400ab308424e
Yoel Horvitz <yoelhor@live.com>
2025-10-10 17:17:05 +0300

    Remove Swager and update packages

--- Program.cs
+++ Program.cs
@@ -21,30 +21,7 @@ builder.Services.Configure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationSch
                                                  });
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Woodgrove Groceries Graph middleware web API",
-        Description = "This dotnet Web API entpoint demonstrate how to secure access to Microsoft Graph via a middleware. Checkout the [source code](https://github.com/microsoft/woodgrove-groceries-graph-middleware) <br> <br> Assembly version " + System.Reflection.Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString() ?? "Unknown version",
-    });
-});
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-{
-    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-    options.RoutePrefix = string.Empty;
-});
-//}
 
 app.UseHttpsRedirection();
 
